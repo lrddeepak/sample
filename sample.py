7aa4935018a520d4ae33414825225097db978a0f
deepak <lrd.deepak@gmail.com>
2016-08-22 19:46:08 +0530

    Updated   envron with sample again
    Deepak

--- sample.py
+++ sample.py
@@ -1,12 +1,12 @@
 from bottle import Bottle, run
 import os
 
-app =Bottle()
+app=Bottle()
 
 
 @app.route('/')
 def index():
     return "Hi there"
 
-PORT = int(os.environ.get('D_PORT', 80))
-run(app, host='0.0.0.0', port=PORT, debug=True)
+PORT = int(os.environ.get('PORT',  '5000'))
+app.run(host='0.0.0.0', port=PORT, debug=True)
