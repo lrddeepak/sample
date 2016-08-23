f20b33040e05880e40076f6996706849a7392ed4
deepak <lrd.deepak@gmail.com>
2016-08-23 16:32:39 +0530

    Change the spelling for definitions in  file  db.py
    Deepak

--- db.py
+++ db.py
@@ -3,30 +3,31 @@ import boto3
 dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 
 table = dynamodb.create_table(
-    TableName = 'users',
-    KeySchema = [
+TableName='users',
+    KeySchema=[
         {
-            'AttributeName' : 'id',
-            'KeyType' : 'HASH'
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
         },
         {
-            'AttributeName' : 'rng',
-            'KeyType' : 'RANGE'
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
         }
     ],
-    AttributeDefinations = [
+    AttributeDefinitions=[
         {
-            'AttributeName' : 'id',
-            'AttributeType' : 's'
+            'AttributeName': 'username',
+            'AttributeType': 'S'
         },
         {
-            'AttributeName' : 'rng',
-            'AttributeType' : 's'
-        }
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+
     ],
     ProvisionedThroughput={
-        'ReadCapacityUnits' : 5,
-        'WriteCapacityUnits' : 5
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
     }
 )
 
